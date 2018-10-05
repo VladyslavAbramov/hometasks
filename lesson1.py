891b6ddf582fa826785747e692fc284cc689420f
Your Name <you@example.com>
2018-10-05 21:59:37 +0300

    tuple

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,7 @@
-print('hi')
+def get_ascii(string):
+    assert type(string) is str
+    arr = list(string)
+    result = []
+    for char in arr:
+        result.append(ord(char))
+    return tuple(result)
